d82a401123e8959b73ec191dd55bc834a8f919a8
Nazar Mazur <nazarii.mazur@gmail.com>
2020-11-03 11:33:06 +0200

    add liggin to proc req

--- studentsdb/context_processors.py
+++ studentsdb/context_processors.py
@@ -1,3 +1,5 @@
 from .settings import PORTAL_URL
+import logging
 def students_proc(request):
+    # logging.info(format("{'*'*10}students_proc(request), {request}"))
     return {'PORTAL_URL': PORTAL_URL}
