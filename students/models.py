cb6d0204b3b1c3cac0ff50ab8f11fa3c053af897
Nazar Mazur <nazarii.mazur@gmail.com>
2020-11-26 18:58:18 +0200

    add sort for groupslist

--- students/models.py
+++ students/models.py
@@ -58,34 +58,34 @@ class Student(models.Model):
 
 
 
-    class Group(models.Model):
-        '''Group Model'''
+class Group(models.Model):
+    '''Group Model'''
 
-        class Meta(object):
-            verbose_name = u"Група"
-            verbose_name_plural = u"Группы"
+    class Meta(object):
+        verbose_name = u"Група"
+        verbose_name_plural = u"Группы"
 
-        title = models.CharField(
-            max_length=256,
-            blank=False,
-            verbose_name=u"Название"
-        )
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Название"
+    )
 
-        leader = models.OneToOneField('Student',
-                                      verbose_name=u"Староста",
-                                      blank=True,
-                                      null=True,
-                                      on_delete=models.SET_NULL)
+    leader = models.OneToOneField('Student',
+                                  verbose_name=u"Староста",
+                                  blank=True,
+                                  null=True,
+                                  on_delete=models.SET_NULL)
 
-        notes = models.TextField(
-            blank=True,
-            verbose_name=u"Дополнительные заметки")
+    notes = models.TextField(
+        blank=True,
+        verbose_name=u"Дополнительные заметки")
 
 
 
-        # Отображает имя группы в админке
-        def __unicode__(self):
-            if self.leader:
-                return u"%s (%s %s)" % (self.title, self.leader.first_name, self.leader.last_name)
-            else:
-                return u"%" % (self.title,)
+    # Отображает имя группы в админке
+    def __unicode__(self):
+        if self.leader:
+            return u"%s (%s %s)" % (self.title, self.leader.first_name, self.leader.last_name)
+        else:
+            return u"%" % (self.title,)
--- students/views/groups.py
+++ students/views/groups.py
@@ -4,22 +4,44 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from ..models import Group
+from ..helpers.pagination import Pagination, EmptyPage, PageNotAnInteger
 
 
 # Views for Grops
 
 def groups_list(request):
-    groups = (
-        {'id': 1,
-         'name': u'MtM-21',
-         'leader': {'id':1, 'name': u'Андрей Корост'}},
-        {'id':2,
-         'name': u'MtM-22',
-         'leader':{'id':2, 'name':u'Назар Мазур'}},
-        {'id': 3,
-         'name': 'MtM-23',
-         'leader': {'id':3, 'name':u'Виталий Подоба'}},
-    )
+    groups = Group.objects.all()
+
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+    #
+    # # set nomber on page
+    #
+    # number_on_page = request.GET.get('number_on_page')
+    # if number_on_page < 1:
+    #     number_on_page = 1
+    #
+    # loading_step = 1
+    #
+    # # paginate students
+    # paginator = Pagination(groups, number_on_page, loading_step)
+    # # paginator = Paginator(students, 3)
+    # page = request.GET.get('page')
+    # try:
+    #     groups = paginator.page(page)
+    # except PageNotAnInteger:
+    #     # If page is not an integer, deliver first page.
+    #     groups = paginator.page(1)
+    #
+    # except EmptyPage:
+    #     # If page is out of range (e.g 9999), deliver last page of resoult
+    #     groups = paginator.page(paginator.num_pages)
+
     return render(request,'students/groups_list.html',
                   {'groups':groups})
 
@@ -28,7 +50,7 @@ def groups_add(request):
     return HttpResponse('<h1>Groups Add Form</h1>')
 
 def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Groups %s</h1>' % gid)
+    return HttpResponse('<h1>Edit Groups %s</h1>' %gid)
 
 def groups_delete(request, gid):
-    return HttpResponse('<h1> Delete Group %s</h1>' % gid)
+    return HttpResponse('<h1> Delete Group %s</h1>' %gid)
