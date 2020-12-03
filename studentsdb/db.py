2c5684d1954b881fab5eaac3398822ba9de0b4d5
Nazar Mazur <nazarii.mazur@gmail.com>
2020-12-03 18:16:24 +0200

    add conf for postgres

--- studentsdb/db.py
+++ studentsdb/db.py
@@ -2,15 +2,32 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'HOST': 'localhost',
-    #     'USER': 'students_db_user',
-    #     'PASSWORD': 'password',
-    #     'NAME': 'students_db',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
     # }
+# 'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#
+#         'NAME': 'students_db',
+#
+#         'USER': 'students_db_user',
+#
+#         'PASSWORD': 'password',
+#
+#         'HOST': 'localhost',
+#
+#         'PORT': '5432',
+#
+#     }
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'students_db_user',
+        'PASSWORD': 'password',
+        'NAME': 'students_db',
+    }
+
 }
