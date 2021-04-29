dd2fa2c3a9915e8ad0358f03ad440a358fef389b
Nazar Mazur <nazarii.mazur@gmail.com>
2021-04-29 10:10:10 +0300

    fixed PremissionRequiredClass

--- students/helpers/login_premissions.py
+++ students/helpers/login_premissions.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import FormView, DeleteView
+from django.views.generic import FormView, DeleteView, View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
 from abc import abstractmethod, ABCMeta
 
-class LoginRequiredClass(FormView):
+class LoginRequiredClass(View):
     ''' Абстарктынй класс для класов вюх который вешает на метод класа вюхи
     dispatch проверку авторизации, унаследуй клас вюхи от этого класса и будет тебе счастье'''
 
@@ -18,7 +18,7 @@ class LoginRequiredClass(FormView):
         return super().dispatch(*args, **kwargs)
 
 
-class PremissionRequiredClass(FormView):
+class PremissionRequiredClass(View):
     ''' Абстарктынй класс для класов вюх который вешает на метод класа вюхи
         dispatch проверку прав доступа авторизвоаного, унаследуй клас вюхи от этого класса и будет тебесчастье'''
     # __metaclass__ = ABCMeta
@@ -28,13 +28,13 @@ class PremissionRequiredClass(FormView):
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
-class PremissionRequiredClass1(DeleteView):
-    ''' Абстарктынй класс для класов вюх который вешает на метод класа вюхи
-        dispatch проверку прав доступа авторизвоаного, унаследуй клас вюхи от этого класса и будет тебесчастье'''
-
-    # __metaclass__ = ABCMeta
-
-    # @abstractmethod
-    @method_decorator(permission_required('auth.add_user', raise_exception=True))
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
+# class PremissionRequiredClass1(DeleteView):
+#     ''' Абстарктынй класс для класов вюх который вешает на метод класа вюхи
+#         dispatch проверку прав доступа авторизвоаного, унаследуй клас вюхи от этого класса и будет тебесчастье'''
+#
+#     # __metaclass__ = ABCMeta
+#
+#     # @abstractmethod
+#     @method_decorator(permission_required('auth.add_user', raise_exception=True))
+#     def dispatch(self, *args, **kwargs):
+#         return super().dispatch(*args, **kwargs)
--- students/views/students.py
+++ students/views/students.py
@@ -18,7 +18,7 @@ from ..util import paginate, get_current_group
 from django.utils.translation import ugettext as _
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from ..helpers.login_premissions import LoginRequiredClass, PremissionRequiredClass, PremissionRequiredClass1
+from ..helpers.login_premissions import LoginRequiredClass, PremissionRequiredClass
 
 
 
@@ -230,7 +230,7 @@ def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' %sid)
 
 
-class StudentDeleteView(PremissionRequiredClass1):
+class StudentDeleteView(DeleteView, PremissionRequiredClass):
 # class StudentDeleteView(DeleteView):
     model = Student
     template_name = 'students/students_confirm_delete.html'
@@ -241,7 +241,7 @@ class StudentDeleteView(PremissionRequiredClass1):
     #     return super(StudentDeleteView, self).dispatch(*args, **kwargs)
 
     def get_success_url(self):
-        return u'%s?status_message=%s' % (reverse('home'), _(u"Student updated successfully"))
+        return '%s?status_message=%s' % (reverse('home'), _("Student updated successfully"))
 
 # def students_delete(request, sid):
 #     return HttpResponse('<h1>Delete Student %s</h1>' %sid)
